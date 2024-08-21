49a5837120058ef4d4800f8e07313029997755eb
Demon <singhayush6264@gmail.com>
2024-08-21 15:49:02 +0530

    Update 21_aug.py

--- 21_aug.py
+++ 21_aug.py
@@ -1,5 +1,12 @@
-a="hello"
+a="  hello ww  1"
 str="12345"
 print(a.upper())
 print(a.lower())
+print("reverse order: "+str[::-1])
 print("reverse order: ",str[::-1])
+x="/home/demon/Desktop/New Folder/codes_in_ubuntu/Python-Mca"
+x1="/home/demon/Downloads"
+print(x)
+print(x1)
+# strip and find method 
+print(a.strip())
