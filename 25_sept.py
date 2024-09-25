e3dfd399a6d627fce02b4cbc40c2d32c46c993b1
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-25 15:33:34 +0530

    commit

---
+++ 25_sept.py
@@ -0,0 +1,14 @@
+# wap to estimate the runtime for all the function that u have used so far
+
+import time as t
+from email_generator import gen
+x=t.ctime()
+
+
+
+sapids = [123,234324,345345,4545]
+
+gen(sapids)
+x1=t.ctime()
+print(x1,x)
+
