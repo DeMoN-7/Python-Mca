051ef4d43fe76e51d8464993bb981f200f246521
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-25 15:48:00 +0530

    commit

--- 25_sept.py
+++ 25_sept.py
@@ -1,14 +1,16 @@
 # wap to estimate the runtime for all the function that u have used so far
 
 import time as t
+from calc import add
 from email_generator import gen
-x=t.ctime()
 
-
-
+x=t.time()
 sapids = [123,234324,345345,4545]
-
 gen(sapids)
-x1=t.ctime()
-print(x1,x)
-
+x1=t.time()
+print((x1-x)*1000000)
+print("2")
+x=t.time()
+print(add(11,313131313))
+x1=t.time()
+print((x1-x)*1000000)
